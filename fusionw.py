c5d3d8378d1ba64424f5888ed1a491625f478cdf
hyf015 <hyf015@gmail.com>
2018-07-29 21:47:59 +0900

    batch_size

--- fusionw.py
+++ fusionw.py
@@ -422,8 +422,8 @@ if __name__ == '__main__':
     del model, optimizer
     del modelw, optimizerw
     from data.lateDataset import lateDatasetTrain, lateDatasetVal
-    train_loader = DataLoader(dataset = lateDatasetTrain, batch_size = 32, shuffle=False, num_workers=0, pin_memory=True)
-    val_loader = DataLoader(dataset = lateDatasetVal, batch_size = 32, shuffle=False, num_workers=0, pin_memory=True)
+    train_loader = DataLoader(dataset = lateDatasetTrain, batch_size = args.batch_size, shuffle=False, num_workers=0, pin_memory=True)
+    val_loader = DataLoader(dataset = lateDatasetVal, batch_size = args.batch_size, shuffle=False, num_workers=0, pin_memory=True)
     trainprev = 999
     valprev = 999
     loss_train = []
