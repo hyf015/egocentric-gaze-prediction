d55325020a848ccaf6003d66dd462c4731ea160d
hyf <hyf@fra.hci>
2018-08-11 15:48:48 +0900

    v1.0

--- fusionw.py
+++ fusionw.py
@@ -22,19 +22,20 @@ print('importing done!')
 parser = argparse.ArgumentParser()
 parser.add_argument('--lr', type=float, default=1e-7, required=False, help='lr for Adam')
 parser.add_argument('--late_save_img', default='loss_late.png', required=False)
-parser.add_argument('--pretrained_model', default='../savefusion/00004_fusion3d_bn_floss_checkpoint.pth.tar', required=False)
-parser.add_argument('--pretrained_lstm', default='../savelstm/best_lstmnet.pth.tar', required=False)
-parser.add_argument('--pretrained_late', default='../savelate/best.pth.tar', required=False)
+parser.add_argument('--pretrained_model', default='save/best_fusion.pth.tar', required=False)
+parser.add_argument('--pretrained_lstm', default='save/valbest_lstm.pth.tar', required=False)
+parser.add_argument('--pretrained_late', default='save/best_late.pth.tar', required=False)
 parser.add_argument('--lstm_save_img', default='loss_lstm.png', required=False)
 parser.add_argument('--save_lstm', default='best_lstm.pth.tar', required=False)
 parser.add_argument('--save_late', default='best_late.pth.tar', required=False)
-parser.add_argument('--save_path', default='../savelate', required=False)
+parser.add_argument('--save_path', default='save', required=False)
 parser.add_argument('--loss_function', default='f', required=False)
 parser.add_argument('--num_epoch', type=int, default=10, required=False)
-parser.add_argument('--extract_lstm', type=bool, default=False, required=False)
-parser.add_argument('--train_lstm', type=bool, default=False, required=False)
-parser.add_argument('--train_late', type=bool, default=False, required=False)
-parser.add_argument('--extract_late', type=bool, default=False, required=False)
+parser.add_argument('--num_epoch_lstm', type=int, default=30, required=False)
+parser.add_argument('--extract_lstm', action='store_true')
+parser.add_argument('--train_lstm', action='store_true')
+parser.add_argument('--train_late', action='store_true')
+parser.add_argument('--extract_late', action='store_true')
 parser.add_argument('--extract_late_pred_folder', default='../new_pred/', required=False)
 parser.add_argument('--extract_late_feat_folder', default='../new_feat/', required=False)
 parser.add_argument('--device', default='0')
@@ -58,7 +59,7 @@ def crop_feature(feature, maxind, size):
     W = feature.size(3)
     for b in range(feature.size(0)):
         fmax = np.array(maxind[b])
-        fmax = fmax / 16  #downsize from 224 to 14
+        fmax = fmax // 16  #downsize from 224 to 14
         fmax = np.clip(fmax, size//2, H-int(math.ceil(size/2.0)))
         cfeature = feature[b,:,(fmax[0]-size//2):(fmax[0]+int(math.ceil(size/2.0))),(fmax[1]-size//2):(fmax[1]+int(math.ceil(size/2.0)))]
         cfeature = cfeature.unsqueeze(0)
@@ -83,7 +84,7 @@ def crop_feature_var(feature, maxind, size):
             res = torch.cat((res, cfeature),0)
     return res
 
-def trainw(epoch, st_loader, modelw, criterion, optimizer):
+def trainw(st_loader, modelw, criterion, optimizer):
     losses = AverageMeter()
     modelw.train()
     hidden = None
@@ -114,7 +115,7 @@ def trainw(epoch, st_loader, modelw, criterion, optimizer):
         
     return losses.avg
 
-def testw(epoch, st_loader, modelw, criterion):
+def testw(st_loader, modelw, criterion):
     losses = AverageMeter()
     modelw.train()
     hidden = None
@@ -239,7 +240,7 @@ def get_weighted(chn_weight, feature):
     #feature = feature - torch.mean(feature)
     return feature
 
-def extract_late(epoch, st_loader, model, modelw, pred_folder=args.extract_late_pred_folder, feat_folder=args.extract_late_feat_folder):
+def extract_late(st_loader, model, modelw, pred_folder=args.extract_late_pred_folder, feat_folder=args.extract_late_feat_folder):
     # pred is the gaze prediction result of SP, feat is the output of AT.
     if not os.path.exists(pred_folder):
         os.makedirs(pred_folder)
@@ -356,8 +357,8 @@ def val_late(epoch, loader, model, criterion):
 
 if __name__ == '__main__':
 
-    #trained_model = 'savelstm/3layerall/0net.pth.tar'
-    print('building pretrained model ...')
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
     model = VGG_st_3dfuse(make_layers(cfg['D'], 3), make_layers(cfg['D'], 20))
     pretrained_dict = torch.load(args.pretrained_model)
     model_dict = model.state_dict()
@@ -385,6 +386,34 @@ if __name__ == '__main__':
         model_dict = modelw.state_dict()
         model_dict.update(pretrained_dict)
         modelw.load_state_dict(model_dict)
+        print('loaded pretrained lstm from ' + args.pretrained_lstm)
+    else:
+        if args.extract_lstm:
+            extract_LSTM_training_data(save_path='../512w', trained_model='save/best_fusion.pth.tar', device='0', crop_size=3)
+        from data.wdatas import wTrainData, wValData
+        wTrainLoader = DataLoader(dataset=wTrainData, batch_size=1, shuffle=False, num_workers=0)
+        wValLoader = DataLoader(dataset=wValData, batch_size=1, shuffle=False, num_workers=0)
+    
+        print('begin training lstm....')
+        prev = 999
+        prevt = 999
+        loss_train = []
+        loss_val = []
+        for epoch in tqdm(range(args.num_epoch_lstm)):
+            #lr = raw_input('please input lr:')
+            #lr = float(lr)
+            #adjust_learning_rate(optimizerw, epoch, lr)
+            l = trainw(wTrainLoader, modelw, criterionw, optimizerw)
+            loss_train.append(l)
+            if l < prevt:
+                torch.save(modelw.state_dict(), os.path.join(args.save_path, args.save_lstm))
+            l = testw(wValLoader, modelw, criterionw)
+            loss_val.append(l)
+            if l<prev:
+                prev=l
+                torch.save(modelw.state_dict(), os.path.join(args.save_path, 'val'+args.save_lstm))
+            plot_loss(loss_train, loss_val, os.path.join(args.save_path, args.lstm_save_img))
+        print('lstm training finished!')
 
     load_late = False
     model_late = late_fusion()
@@ -395,50 +424,14 @@ if __name__ == '__main__':
         model_dict = model_late.state_dict()
         model_dict.update(pretrained_dict)
         model_late.load_state_dict(model_dict)
-        del pretrained_dict
 
     optimizer_late = torch.optim.Adam(model_late.parameters(), lr=1e-4)
-    print('init done!')
     
     #vis_features(STTrainLoader, model, modelw, 'savelstm/3layerall/vistrainrelu/')
-    if not os.path.exists(args.save_path):
-        os.makedirs(args.save_path)
-    if args.train_lstm:
-        if args.extract_lstm:
-            extract_LSTM_training_data(save_path='../512w', trained_model='save/best_fusion.pth.tar', device='0', crop_size=3)
-        from data.wdatas import wTrainData, wValData
-        wTrainLoader = DataLoader(dataset=wTrainData, batch_size=1, shuffle=False, num_workers=0)
-        wValLoader = DataLoader(dataset=wValData, batch_size=1, shuffle=False, num_workers=0)
-    
-        print('begin training lstm....')
-        #trainw(0, wTrainLoader, modelw, criterionw, optimizerw, val=True)
-        #trainw(0, wTrainLoader, modelw, criterionw, optimizerw)
-        prev = 999
-        prevt = 999
-        loss_train = []
-        loss_val = []
-        for epoch in tqdm(range(120)):
-            #lr = raw_input('please input lr:')
-            #lr = float(lr)
-            #adjust_learning_rate(optimizerw, epoch, lr)
-            l = trainw(epoch, wTrainLoader, modelw, criterionw, optimizerw)
-            loss_train.append(l)
-            if l < prevt:
-                torch.save(modelw.state_dict(), os.path.join(args.save_path, args.save_lstm))
-            l = testw(0, wValLoader, modelw, criterionw, optimizerw)
-            loss_val.append(l)
-            if l<prev:
-                prev=l
-                torch.save(modelw.state_dict(), os.path.join(args.save_path, 'val'+args.save_lstm))
-        plot_loss(loss_train, loss_val, os.path.join(args.save_path, args.lstm_save_img))
-    print('lstm training finished!')
 
     if args.extract_late:
-        extract_late(0, DataLoader(dataset=STValData, batch_size=1, shuffle=False, num_workers=1, pin_memory=True), model, modelw)
-        extract_late(0, DataLoader(dataset=STTrainData, batch_size=1, shuffle=False, num_workers=1, pin_memory=True), model, modelw)
-
-    #STTrainLoader = DataLoader(dataset=STTrainData, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
-    #STValLoader = DataLoader(dataset=STValData, batch_size=1, shuffle=False, num_workers=1, pin_memory=True)
+        extract_late(DataLoader(dataset=STValData, batch_size=1, shuffle=False, num_workers=1, pin_memory=True), model, modelw)
+        extract_late(DataLoader(dataset=STTrainData, batch_size=1, shuffle=False, num_workers=1, pin_memory=True), model, modelw)
 
     del model, optimizer
     del modelw, optimizerw
