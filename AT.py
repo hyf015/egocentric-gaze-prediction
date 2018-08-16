26f0bcecf3084aad64739270c6ffae8f58c4125d
hyf015 <hyf015@gmail.com>
2018-08-17 07:28:39 +0900

    minor fix

--- AT.py
+++ AT.py
@@ -83,9 +83,9 @@ class AT():
         self.model.load_state_dict(model_dict, strict=False)
         self.model.to(self.device)
         self.model._modules.get(hook_name).register_forward_hook(hook_feature)
-        from data.LSTMdatas import LSTMdataset
-        self.lstmTrainLoader = DataLoader(dataset=LSTMdataset(os.path.join(lstm_data_path, 'train')), batch_size=1, shuffle=False, num_workers=0)
-        self.lstmValLoader = DataLoader(dataset=LSTMdataset(os.path.join(lstm_data_path, 'test')), batch_size=1, shuffle=False, num_workers=0)
+        from data.LSTMdatas import lstmDataset
+        self.lstmTrainLoader = DataLoader(dataset=lstmDataset(os.path.join(lstm_data_path, 'train')), batch_size=1, shuffle=False, num_workers=0)
+        self.lstmValLoader = DataLoader(dataset=lstmDataset(os.path.join(lstm_data_path, 'test')), batch_size=1, shuffle=False, num_workers=0)
 
     def trainLSTM(self):
         losses = AverageMeter()
