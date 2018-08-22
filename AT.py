8d34552a1768f68b851608b7f12ad380bd0e2727
hyf015 <hyf015@gmail.com>
2018-08-22 15:04:28 +0900

    indent using spaces

--- AT.py
+++ AT.py
@@ -90,12 +90,12 @@ class AT():
         self.lstmValLoader = DataLoader(dataset=lstmDataset(os.path.join(lstm_data_path, 'test')), batch_size=1, shuffle=False, num_workers=0)
 
     def reload_LSTM(self, pretrained_lstm):
-    	pretrained_dict = torch.load(pretrained_lstm)
+        pretrained_dict = torch.load(pretrained_lstm)
         model_dict = self.lstm.state_dict()
         model_dict.update(pretrained_dict)
         self.lstm.load_state_dict(model_dict)
         print('loaded pretrained lstm from ' + pretrained_lstm)
-    	
+        
 
     def trainLSTM(self):
         losses = AverageMeter()
